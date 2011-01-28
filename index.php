529951b0ee00497c10f35aee734759e3c1e80589
Eric Gelinas <eric@standardpixel.com>
2011-01-28 06:29:34 +0000

    separate controller code from view

--- index.php
+++ index.php
@@ -2,7 +2,16 @@
 include('db.txt');
 $cocktail_date = strtotime($db[0]['date']);
 $friday_date   = strtotime('next Friday');
-$today_date    = strtotime('Today')
+$today_date    = strtotime('Today');
+$unfriendly_date = date('M, d Y',$cocktail_date);
+
+if($friday_date==$cocktail_date) {
+	$friendly_date = 'This Friday';
+} elseif($today_date==$cocktail_date) {
+	$friendly_date = 'Today';
+} else {
+	$friendly_date = 'On:&nbsp;' . $unfriendly_date;
+}
 ?>
 <!DOCTYPE html>
 
@@ -16,8 +25,22 @@ $today_date    = strtotime('Today')
 </head>
 <body>
 
-	<div class='body'>What are we drinking <?if($friday_date==$cocktail_date){?>This Friday<?}elseif($today_date==$cocktail_date){?>Today<?}else{?>On: <?=date('M, d Y',$cocktail_date)?><?}?>? <?if($db[0]['url']){?><a href="<?=$db[0]['url']?>" class="hot"><?}?><span class="hot"><?=$db[0]['title']?></span><?if($db[0]['url']){?></a><?}?></div>
+	<div class='body'>
+		
+		What are we drinking <?= $friendly_date ?>?
+		
+		<?if($db[0]['url']){?>
+			<a href="<?=$db[0]['url']?>" class="hot">
+		<?}?>
+		
+		<span class="hot"><?=$db[0]['title']?></span>
+		
+		<?if($db[0]['url']){?>
+			</a>
+		<?}?>
+		
+	</div>
 
-<div class='footer'>Barkeep: <?=$db[0]['barkeep']?></div>
+	<div class='footer'>Barkeep: <?=$db[0]['barkeep']?></div>
 </body>
 </html>
